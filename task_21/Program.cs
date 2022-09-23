a750ccd0719475b1e1b342de06ea4de33ae49f70
Your Name <you@example.com>
2022-09-23 19:28:19 +0300

    HomeWork_#3

---
+++ task_21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5,0); B (1,-1,9) -> 11.53
+
+using static System.Console;
+Clear();
+Write("Введите координату X точки А: ");
+int Ax = Convert.ToInt32(ReadLine());
+Write("Введите координату Y точки А: ");
+int Ay = Convert.ToInt32(ReadLine());
+Write("Введите координату Z точки А: ");
+int Az = Convert.ToInt32(ReadLine());
+Write("Введите координату X точки B: ");
+int Bx = Convert.ToInt32(ReadLine());
+Write("Введите координату Y точки B: ");
+int By = Convert.ToInt32(ReadLine());
+Write("Введите координату Z точки B: ");
+int Bz = Convert.ToInt32(ReadLine());
+double length = Math.Sqrt (Math.Pow (Ax-Bx,2) + Math.Pow (Ay-By,2) + Math.Pow (Az-Bz,2));
+WriteLine($"{length:f2}");
---
+++ task_23/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) и
+// выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+using static System.Console;
+Clear();
+found:
+
+WriteLine("Введите число: ");
+int num = Convert.ToInt32(ReadLine());
+int idx = 1;
+if (num == 0)
+{
+    WriteLine("Вы ввели 0, пожалуйста введите другое число ");
+
+    goto found;
+}
+Write($"{num} -> ");
+while (idx<num)
+{
+
+    Write($"{Math.Pow(idx,3)}, ");
+    idx++;
+}
+if (idx==num)
+{
+    Write($"{Math.Pow(idx,3)}");
+}
